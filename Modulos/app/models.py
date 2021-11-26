f42d7e4d112883ab59b5d39b41fc6bf8b14e1417
Rinvarito <bastiancamus77@gmail.com>
2021-11-26 11:23:28 -0300

    cambios

--- Modulos/app/models.py
+++ Modulos/app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Usuario(models.Model):
     usuario = models.CharField(max_length=20, primary_key=True)
-    contraseña = models.CharField(max_length=20,primary_key=True)
-    correo = models.CharField(max_length=100,primary_key=True)
-    edad = models.CharField(max_length=3,primary_key=True)
-    apodo = models.Charfield(max_length=20, primary_key=True)
+    contraseña = models.CharField(max_length=20,)
+    correo = models.CharField(max_length=100,)
+    edad = models.CharField(max_length=3,)
+    apodo = models.CharField(max_length=20,)
 
     def infouser(self):
         txt = "{0},{1},{2},{3},{4}"
--- Modulos/app/views.py
+++ Modulos/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'login.html'
+
